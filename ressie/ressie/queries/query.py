0500eb5b51d7f2ba8029fdef32d318ae13ca831e
Lovro Predovan <lovro.predovan@gmail.com>
2017-01-15 15:01:08 +0100

    Added sample elastic query

--- ressie/ressie/queries/query.py
+++ ressie/ressie/queries/query.py
@@ -1,6 +1,7 @@
 import os
 
 import whoosh.index as index
+from elasticsearch import Elasticsearch
 from whoosh.qparser import QueryParser
 
 
@@ -11,18 +12,29 @@ class ElasticQuery(object):
         pass
 
     def check_status(self):
-        ix = index.open_dir(self.index_folder)
-        qp = QueryParser("attack", schema=ix.schema)
-        q = qp.parse(u"php")
-
-        with ix.searcher() as s:
-            results = s.search(q, limit=20)
-
-            print results
-
+        self.check_elasticsearch()
         '''
         ix = index.open_dir(self.index_dir)
         writer = ix.writer()
 
         writer.commit()
         '''
+
+    def check_attack_db(self, attack):
+        ix = index.open_dir(self.index_folder)
+        qp = QueryParser("attack", schema=ix.schema)
+        q = qp.parse(u"%s" % attack)
+
+        with ix.searcher() as s:
+            s.search(q, limit=20)
+
+    def check_elasticsearch(self):
+        es = Elasticsearch()
+        res = es.search(index="logstash-2017.01.15",
+                        doc_type="logs",
+                        body={"query": {"match_all": {}}})
+
+        for hit in res['hits']['hits']:
+            print("%s" % hit["_source"]["message"])
+
+            # print "aaa"
