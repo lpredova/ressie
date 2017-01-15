9c2825e925be2008942eea6ca0945a48544292f0
Lovro Predovan <lovro.predovan@gmail.com>
2017-01-15 18:19:03 +0100

    added structure for new query

--- ressie/ressie/queries/query.py
+++ ressie/ressie/queries/query.py
@@ -7,13 +7,16 @@ from whoosh.qparser import QueryParser
 
 
 class ElasticQuery(object):
+    # in minutes
+    time_threshold = 200000
     index_folder = os.getcwd() + "/data/index/"
 
     def __init__(self):
         pass
 
     def check_status(self):
-        self.check_elasticsearch()
+        #self.elasticsearch()
+        self.elasticsearch_req_number()
 
     def check_attack_db(self, attack):
         ix = index.open_dir(self.index_folder)
@@ -23,13 +26,13 @@ class ElasticQuery(object):
         with ix.searcher() as s:
             s.search(q, limit=20)
 
-    def check_elasticsearch(self):
+    def elasticsearch(self):
 
         query = {
             "query": {
                 "range":
                     {
-                        "@timestamp": {"gte": "now-20000m"}
+                        "@timestamp": {"gte": "now-%dm" % self.time_threshold}
                     }
             }
         }
@@ -48,4 +51,31 @@ class ElasticQuery(object):
                 # Save to database
 
         except Exception as e:
-            print e.message
+            print(e.message)
+
+    def elasticsearch_req_number(self):
+        query = {
+            "query": {
+                "range":
+                    {
+                        "@timestamp": {"gte": "now-%dm" % self.time_threshold}
+                    }
+            }
+        }
+
+        date = datetime.datetime.now()
+        date = date.strftime("%Y.%m.%d")
+        index_date = "logstash-%s" % date
+        es = Elasticsearch()
+        try:
+            res = es.search(index=index_date, doc_type="logs", body=query)
+            print res
+            for hit in res['hits']['hits']:
+                print("%s" % hit["_source"]["message"])
+
+                # Make comparison for error body od request data
+                # Compare with local database
+                # Save to database
+
+        except Exception as e:
+            print(e.message)
